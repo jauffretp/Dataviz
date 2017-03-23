801754f6bec6bb2bfd8e7a7eeb749b3494842f33
Pascal <jauffret@etud.insa-toulouse.fr>
2017-03-23 17:52:31 +0100

    added doughnut

--- webapp/js/data.js
+++ webapp/js/data.js
@@ -16,7 +16,7 @@ var dataEmploiFamille = {
     ],
     datasets: [
         {
-            data: [80, 497],
+            data: [75, 490],
             backgroundColor: [
                 "#ed0909",
                 "#5894f4"
@@ -29,18 +29,11 @@ var dataEmploiFamille = {
 };
 
 
-var myDoughnutChart = new Chart(ctx, {
-    type: 'pie',
+var pieFamilleCollab = new Chart(ctx, {
+    type: 'doughnut',
     data: dataEmploiFamille,
     options: {
         responsive: true,
-        // animation:{
-        //     animateScale:true
-        // },
-        title: {
-            display: false,
-            text: 'Les députés employant un membre de leur famille'
-        },
         pieceLabel: {
       		// mode 'label', 'value' or 'percentage', default is 'percentage'
       		mode: 'percentage',
@@ -61,3 +54,190 @@ var myDoughnutChart = new Chart(ctx, {
 });
 
 
+
+
+ctx = document.getElementById("pieSexeCollabFamille").getContext("2d");
+
+// Les députés emploient leur femme
+var dataSexeCollabFamille = {
+    labels: [
+        "Femmes",
+        "Hommes",
+    ],
+    datasets: [
+        {
+            data: [54, 21],
+            backgroundColor: [
+                "#ed0909",
+                "#5894f4"
+            ],
+            hoverBackgroundColor: [
+                "#ed0909",
+                "#5894f4"
+            ]
+        }]
+};
+
+var pieSexeCollabFamille = new Chart(ctx, {
+    type: 'doughnut',
+    data: dataSexeCollabFamille,
+    options: {
+        responsive: true,
+        pieceLabel: {
+      		// mode 'label', 'value' or 'percentage', default is 'percentage'
+      		mode: 'percentage',
+      		fontSize: 15,
+      		fontColor: '#330000',
+      		fontFamily: "'Lato', sans-serif",
+      		fontStyle: 'bold'
+
+		},
+		tooltips: {
+			callbacks: {
+			    label: function (tooltipItems, data) {
+			        return  data.datasets[tooltipItems.datasetIndex].data[tooltipItems.index] + ' attaché(e)s parlementaires' ;
+			    }
+			}
+		}	
+    }
+});
+
+
+
+ctx = document.getElementById("pieSexeCollab").getContext("2d");
+
+// Les collaborateurs sont essentiellement des femmes
+var dataPieSexeCollab = {
+    labels: [
+        "Femmes",
+        "Hommes",
+    ],
+    datasets: [
+        {
+            data: [1211, 836],
+            backgroundColor: [
+                "#ed0909",
+                "#5894f4"
+            ],
+            hoverBackgroundColor: [
+                "#ed0909",
+                "#5894f4"
+            ]
+        }]
+};
+
+var pieSexeCollab = new Chart(ctx, {
+    type: 'doughnut',
+    data: dataPieSexeCollab,
+    options: {
+        responsive: true,
+        pieceLabel: {
+      		// mode 'label', 'value' or 'percentage', default is 'percentage'
+      		mode: 'percentage',
+      		fontSize: 15,
+      		fontColor: '#330000',
+      		fontFamily: "'Lato', sans-serif",
+      		fontStyle: 'bold'
+
+		},
+		tooltips: {
+			callbacks: {
+			    label: function (tooltipItems, data) {
+			        return  data.datasets[tooltipItems.datasetIndex].data[tooltipItems.index] + ' attaché(e)s parlementaires' ;
+			    }
+			}
+		}	
+    }
+});
+
+
+ctx = document.getElementById("pieFemmeCollab").getContext("2d");
+
+var dataPieFemmeCollab = {
+    labels: [
+        "Femmes",
+        "Hommes",
+    ],
+    datasets: [
+        {
+            data: [301, 225],
+            backgroundColor: [
+                "#ed0909",
+                "#5894f4"
+            ],
+            hoverBackgroundColor: [
+                "#ed0909",
+                "#5894f4"
+            ]
+        }]
+};
+
+var pieFemmeCollab = new Chart(ctx, {
+    type: 'doughnut',
+    data: dataPieFemmeCollab,
+    options: {
+        responsive: true,
+        pieceLabel: {
+      		// mode 'label', 'value' or 'percentage', default is 'percentage'
+      		mode: 'percentage',
+      		fontSize: 15,
+      		fontColor: '#330000',
+      		fontFamily: "'Lato', sans-serif",
+      		fontStyle: 'bold'
+
+		},
+		tooltips: {
+			callbacks: {
+			    label: function (tooltipItems, data) {
+			        return  data.datasets[tooltipItems.datasetIndex].data[tooltipItems.index] + ' attaché(e)s parlementaires' ;
+			    }
+			}
+		}	
+    }
+});
+
+ctx = document.getElementById("pieHommeCollab").getContext("2d");
+
+ 
+var dataPieHommeCollab = {
+    labels: [
+        "Femmes",
+        "Hommes",
+    ],
+    datasets: [
+        {
+            data: [910, 611],
+            backgroundColor: [
+                "#ed0909",
+                "#5894f4"
+            ],
+            hoverBackgroundColor: [
+                "#ed0909",
+                "#5894f4"
+            ]
+        }]
+};
+
+
+var dataPieHommeCollab = new Chart(ctx, {
+    type: 'doughnut',
+    data: dataPieHommeCollab,
+    options: {
+        responsive: true,
+        pieceLabel: {
+      		// mode 'label', 'value' or 'percentage', default is 'percentage'
+      		mode: 'percentage',
+      		fontSize: 15,
+      		fontColor: '#330000',
+      		fontFamily: "'Lato', sans-serif",
+      		fontStyle: 'bold'
+		},
+		tooltips: {
+			callbacks: {
+			    label: function (tooltipItems, data) {
+			        return  data.datasets[tooltipItems.datasetIndex].data[tooltipItems.index] + ' attaché(e)s parlementaires' ;
+			    }
+			}
+		}	
+    }
+});
