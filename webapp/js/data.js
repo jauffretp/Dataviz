89399a00eea0f8f795e0ae5da3d62c78ad366bc5
Pascal <jauffret@etud.insa-toulouse.fr>
2017-03-23 13:27:32 +0100

    Switched one pie chart to chartjs

---
+++ webapp/js/data.js
@@ -0,0 +1,54 @@
+
+Chart.defaults.global.title.fontSize = 25;
+Chart.defaults.global.title.fontFamily = "'Lato', sans-serif";
+Chart.defaults.global.title.fontColor = '#000000';
+
+//Chart.defaults.global.title.fontSize = 20;
+
+var ctx = document.getElementById("pieFamilleEmployee").getContext("2d");
+
+console.log(ctx)
+
+var dataEmploiFamille = {
+    labels: [
+        "Emploie un membre de leur famille",
+        "N'emploie pas de membre de leur famille",
+    ],
+    datasets: [
+        {
+            data: [13, 87],
+            backgroundColor: [
+                "#ed0909",
+                "#5894f4"
+            ],
+            hoverBackgroundColor: [
+                "#ed0909",
+                "#5894f4"
+            ]
+        }]
+};
+
+
+var myDoughnutChart = new Chart(ctx, {
+    type: 'pie',
+    data: dataEmploiFamille,
+    options: {
+        responsive: true,
+        // animation:{
+        //     animateScale:true
+        // },
+        title: {
+            display: true,
+            text: 'Les députés employant un membre de leur famille'
+        },
+        pieceLabel: {
+      		// mode 'label', 'value' or 'percentage', default is 'percentage'
+      		mode: 'percentage',
+      		fontSize: 30,
+      		fontColor: '#330000'
+
+      }	
+    }
+});
+
+
